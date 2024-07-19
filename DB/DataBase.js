597916450d76b8cdefdb626a81dc9228bdae324e
Felipe Rodrigues Costa <79774048+felipecosta2000@users.noreply.github.com>
2024-07-19 17:35:22 -0300

    DataBase.js

--- DB/DataBase.js
+++ DB/DataBase.js
@@ -1,6 +1,6 @@
 const mongoose = require ('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/dbaula2',
+mongoose.connect('mongodb://localhost:27017/nome-do-banco',
      {useNewUrlParser: true, useUnifiedTopology: true})
 
      .then(()=>console.log('Banco de Dados Open'))
